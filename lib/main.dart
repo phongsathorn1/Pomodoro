0476bfb2131f9d6c53b6ff47ba53091f51a4d3f7
eieieueueoeo <naruebetcm@gmail.com>
2019-03-28 05:10:40 +0700

    First commit
    Search function and pass values to next page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:pomodoro/detailsPage.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -9,103 +13,169 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => new ExamplePage(),
+        '/moreDetail': (BuildContext context) => new DetailsPage(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ExamplePage extends StatefulWidget{
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ExamplePageState createState() => _ExamplePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+class _ExamplePageState extends State<ExamplePage>{
+  
+  final TextEditingController _filter = new TextEditingController();
+  //final dio = new Dio();
+  String _searchText = "";
+  List names = new List();
+  List filteredName = new List();
+  final dio = new Dio();
+  Icon _searchIcon = new Icon(Icons.search);
+  Widget _appBarTitle = new Text("Search Example");
+  
+
+  _ExamplePageState(){
+    _filter.addListener((){
+      if (_filter.text.isEmpty){
+        setState( (){
+          _searchText = "";
+          filteredName = names;
+        });
+      }
+      else{
+        setState(() {
+         _searchText = _filter.text; 
+        });
+      }
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  void initState(){
+    this._getName();
+    super.initState();
+  }
+
+  void _getName() async{
+    List tempList = new List();
+    final response = await dio.get('https://swapi.co/api/people');
+    //final response = json.decode(await new File('/resource/data.json').readAsString());
+    print(response.data['result']);
+    for (int i = 0; i < response.data['results'].length; i++) {
+      tempList.add(response.data['results'][i]);
+    }
+
+    setState(() {
+      names = tempList;
+      filteredName = names;
+    });
+  }
+
+  void _searchPressed(){
+    setState(() {
+     if (this._searchIcon.icon == Icons.search){
+       this._searchIcon = new Icon(Icons.close);
+       this._appBarTitle = new TextField(
+         controller: _filter,
+         decoration: new InputDecoration(
+           prefixIcon: new Icon(Icons.search),
+           hintText: 'Search...'
+         ),
+       );
+     }
+     else{
+       this._searchIcon = new Icon(Icons.search);
+       this._appBarTitle = new Text("Search Example");
+       filteredName = names;
+       _filter.clear();
+     }
+    });
+  }
+  
+  Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: _buildBar(context),
+      body: Container(
+        child: _bulidList(),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      resizeToAvoidBottomPadding: false,
     );
   }
+
+  Widget _buildBar(BuildContext context){
+    return new AppBar(
+      centerTitle: true,
+      title: _appBarTitle,
+      leading: new IconButton(
+        icon: _searchIcon,
+        onPressed: _searchPressed,
+      ),
+    );
+  }
+
+  Widget _bulidList(){
+    if (! (_searchText.isEmpty)){
+      List tempList = new List();
+      for (int i = 0; i<filteredName.length;i++){
+        if (filteredName[i]['name'].toLowerCase().contains(_searchText.toLowerCase())){
+          tempList.add(filteredName[i]);
+        }
+      }
+      filteredName = tempList;
+    }
+
+    var imgTemp = new Image.asset(
+      'resource/temp.png',
+      height: 200,
+    );
+
+    return ListView.builder(
+      itemCount: names == null? 0 : filteredName.length,
+      itemBuilder: (BuildContext context, int index){
+        return new Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              imgTemp,
+              Row(
+                children: <Widget>[
+                  Icon(Icons.album),
+                  Text(filteredName[index]['name']),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.alarm),
+                  Text("09.30 - 12.30"),
+                ],
+              ),
+              RaisedButton(
+                child: Text('more details'),
+                color: Colors.redAccent,
+                textColor: Colors.white,
+                onPressed: (){
+                  var route = new MaterialPageRoute(
+                    builder: (BuildContext context)
+                      => new DetailsPage(value : filteredName[index]),
+                  );
+                  Navigator.of(context).push(route);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
 }
+
