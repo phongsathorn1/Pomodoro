9f9797f593cd3ad1c55940a32ee14dbd8509f3c7
Phongsathorn Kittiworapanya <Phongsathron@outlook.com>
2019-04-16 00:20:37 +0700

    Add welcome screen

---
+++ lib/views/welcome.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          padding: EdgeInsets.symmetric(vertical: 40),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Text("Welcome to Pomodoro app",
+                  style: TextStyle(
+                    fontSize: 20
+                  ),
+                ),
+                RaisedButton(
+                  child: Text("Login"),
+                  onPressed: () => {Navigator.pushNamed(context, '/login')},
+                ),
+                RaisedButton(
+                  child: Text("Register"),
+                  onPressed: () => {Navigator.pushNamed(context, '/register')},
+                ),
+                FlatButton(
+                  child: Text("Forgot password"),
+                  onPressed: () => {
+                    Navigator.pushNamed(context, '/forgot')
+                  },
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
