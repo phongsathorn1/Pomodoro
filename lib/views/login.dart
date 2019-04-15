11bc7c73e8c1044d5d7df7d66825d0d8442c0fa6
Phongsathorn Kittiworapanya <Phongsathron@outlook.com>
2019-04-16 00:19:58 +0700

    Add firebase auth login

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
 
 class LoginScreen extends StatefulWidget{
   @override
@@ -27,12 +30,58 @@ class LoginScreenState extends State<LoginScreen>{
               decoration: InputDecoration(
                 labelText: "Email"
               ),
+              validator: (value) {
+                if(value.isEmpty){
+                  return "Please fill email";
+                }
+              },
             ),
             TextFormField(
               controller: _password,
               decoration: InputDecoration(
                 labelText: "Password"
               ),
+              obscureText: true,
+              autocorrect: false,
+              validator: (value) {
+                if(value.isEmpty){
+                  return "Plesae fill password";
+                }
+              },
+            ),
+            RaisedButton(
+              child: Text("Login"),
+              onPressed: () async {
+                if(_formKey.currentState.validate()){
+                  try {
+                    FirebaseUser user = await _auth.signInWithEmailAndPassword(
+                      email: _email.text.trim(),
+                      password: _password.text.trim()
+                    );
+                    
+                    Navigator.pushNamed(context, '/');
+                  }
+                  catch (e) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context){
+                        return AlertDialog(
+                          title: Text("Login"),
+                          content: Text("Login Falied"),
+                          actions: <Widget>[
+                            FlatButton(
+                              child: Text("Done"),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            )
+                          ],
+                        );
+                      }
+                    );
+                  }
+                }
+              },
             )
           ]),
         )
