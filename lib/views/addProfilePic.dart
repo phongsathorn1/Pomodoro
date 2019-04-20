1a708b33f012aafd9e4f181a1c3bed93f2fe4cb8
Phongsathorn Kittiworapanya <Phongsathron@outlook.com>
2019-04-21 00:05:03 +0700

    Add user avatar

--- lib/views/addProfilePic.dart
+++ lib/views/addProfilePic.dart
@@ -14,7 +14,7 @@ class AddProfilePicScreenState extends State<AddProfilePicScreen> {
   File _image;
 
   Future getImage() async {
-    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
 
     setState(() {
       _image = image;
